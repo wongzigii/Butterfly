b67c9dc53e10f8598f5c59c92f1e5a972b94dadb
Wongzigii <wongzigii@outlook.com>
2015-07-18 16:53:18 +0800

    Refactor project with Pod

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 3
+
+// Butterfly
+#define COCOAPODS_POD_AVAILABLE_Butterfly
+#define COCOAPODS_VERSION_MAJOR_Butterfly 0
+#define COCOAPODS_VERSION_MINOR_Butterfly 3
+#define COCOAPODS_VERSION_PATCH_Butterfly 4
+
