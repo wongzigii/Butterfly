522f77e9a5ed281990a8f3dbe9e05571e483f4a4
Wong Zigii <wongzigii@outlook.com>
2015-12-12 22:23:20 +0800

    Redirect Butterfly file's path

---
+++ Example/Butterfly-Demo/ButterflyDrawView.swift
@@ -0,0 +1,186 @@
+//
+//  ButterflyDrawView.swift
+//  Butterfly
+//
+//  Created by Zhijie Huang on 15/6/20.
+//
+//  Copyright (c) 2015 Zhijie Huang <wongzigii@outlook.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+import CoreGraphics
+
+///
+//	Protocol of `ButterflyDrawView`.
+//
+
+protocol ButterflyDrawViewDelegate: class {
+    ///
+    /// Called when start drawing in ButterflyDrawView
+    ///
+    func drawViewDidStartDrawingInView(drawView: ButterflyDrawView?)
+    
+    ///
+    /// Called when end drawing in ButterflyDrawView
+    ///
+    func drawViewDidEndDrawingInView(drawView: ButterflyDrawView?)
+}
+
+///
+// ButterflyDrawView Class.
+//
+internal class ButterflyDrawView : UIView {
+    
+    /// Line width of the drawing path.
+    var lineWidth: Float?
+    
+    /// Line color of the drawing path.
+    var lineColor: UIColor?
+    
+    weak var delegate: ButterflyDrawViewDelegate?
+    
+    var isTouchBegan: Bool?
+    
+    private var backgroundImageView: UIImageView?
+    
+    private var path: UIBezierPath?
+    
+    private var previousPoint: CGPoint?
+    
+    private var oldPoint: CGPoint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.mainScreen().bounds)
+        lineWidth = 3.0
+        lineColor = UIColor.redColor()
+        setup()
+    }
+    
+    // Use this initializer to custom lineWidth and lineColor if need.
+    
+    convenience init(lineWidth: Float, lineColor: UIColor) {
+        self.init(frame: UIScreen.mainScreen().bounds)
+        self.lineWidth = lineWidth
+        self.lineColor = lineColor
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented\n")
+    }
+    
+    private func setup() {
+        backgroundColor = UIColor.clearColor()
+        
+        backgroundImageView = UIImageView(frame: self.bounds)
+        addSubview(backgroundImageView!)
+        backgroundImageView?.autoresizingMask = UIViewAutoresizing([.FlexibleHeight, .FlexibleWidth])
+        backgroundImageView?.contentMode = UIViewContentMode.Center
+        
+        path = UIBezierPath()
+        if let lineWidth = self.lineWidth {
+            path?.lineWidth = CGFloat(lineWidth)
+        }
+        path?.lineCapStyle = CGLineCap.Round
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isTouchBegan = true
+        breakOutPath()
+        if let touch = touches.first {
+            previousPoint = touch.locationInView(self)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let delegate = self.delegate {
+            delegate.drawViewDidStartDrawingInView(self)
+            isTouchBegan = false
+        }
+        
+        if let touch = touches.first {
+            oldPoint = previousPoint
+            previousPoint = touch.previousLocationInView(self)
+            let currentPoint: CGPoint = touch.locationInView(self)
+            
+            let midPoint1 = CGPoint(
+                x: (oldPoint!.x + previousPoint!.x) / 2,
+                y: (oldPoint!.y + previousPoint!.y) / 2)
+            let midPoint2 = CGPoint(
+                x: (previousPoint!.x + currentPoint.x) / 2,
+                y: (previousPoint!.y + currentPoint.y) / 2)
+            
+            path?.moveToPoint(midPoint1)
+            path?.addQuadCurveToPoint(midPoint2, controlPoint: previousPoint!)
+            
+            setNeedsDisplay()
+        }
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        cache()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        cache()
+        if let delegate = self.delegate {
+            delegate.drawViewDidEndDrawingInView(self)
+        }
+    }
+    
+    /// Cache for the current image.
+    
+    private func cache() {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0)
+        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        backgroundImageView?.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetAllowsAntialiasing(context, true)
+        CGContextSetShouldAntialias(context, true)
+        
+        lineColor?.setStroke()
+        path?.stroke()
+    }
+    
+    private func breakOutPath() {
+        path?.removeAllPoints()
+    }
+    
+    func clear() {
+        breakOutPath()
+        backgroundImageView?.image = nil
+        setNeedsDisplay()
+    }
+    
+    func disable() {
+        self.userInteractionEnabled = false
+    }
+    
+    func enable() {
+        self.userInteractionEnabled = true
+    }
+    
+    deinit {
+        self.delegate = nil
+    }
+}
---
+++ Example/Butterfly-Demo/ButterflyManager.swift
@@ -0,0 +1,177 @@
+//
+//  ButterflyManager.swift
+//  Butterfly
+//
+//  Created by Zhijie Huang on 15/6/20.
+//
+//  Copyright (c) 2015 Zhijie Huang <wongzigii@outlook.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+
+public let ButterflyDidShakingNotification = "com.wongzigii.Butterfly.ShakingNotification"
+
+private let instance = ButterflyManager()
+
+
+///  Main manager class of Butterfly
+
+public class ButterflyManager: NSObject, ButterflyViewControllerDelegate {
+    
+    /// You can access instance variable `imageWillUpload` directly.
+    public var imageWillUpload: UIImage? {
+        return self.butterflyViewController?.imageWillUpload
+    }
+    
+    /// Or instance variable `textWillUploader`.
+    public var textWillUpload: String? {
+        return self.butterflyViewController?.textWillUpload
+    }
+    
+    /// Manager is listening shake event or not.
+    public var isListeningShake: Bool?
+    
+    /// Shared manager used by the extension across Butterfly.
+    public class var sharedManager: ButterflyManager {
+        return instance
+    }
+    
+    /// ViewController instance used by this manager.
+    var butterflyViewController: ButterflyViewController?
+    
+    /// Register and start listening shake event.
+    /// Register this method in AppDelegate will listen all motions during the whole application's life cycle.
+    public func startListeningShake() {
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "handleShake:",
+            name: ButterflyDidShakingNotification,
+            object: nil)
+        isListeningShake = true
+    }
+    
+    
+    /// Unregister and stop listening shake event.
+    /// Optional: you can just listen the specific one (viewController) you want.
+    public func stopListeningShake() {
+        NSNotificationCenter.defaultCenter().removeObserver(
+            self,
+            name: ButterflyDidShakingNotification,
+            object: nil)
+        isListeningShake = false
+    }
+    
+    /// ButterflyViewController delegate method. Will be invoked when send button pressed. You may want to implement this method to handle the image.
+    ///
+    func ButterflyViewControllerDidPressedSendButton(drawView: ButterflyDrawView?) {
+        /// NOTE: Custom this method for further uploading.
+        ///
+        /// That would be a great idea to upload your useful application information here manually .
+        if let image = imageWillUpload {
+            let data: UIImage = image
+            ButterflyFileUploader.sharedUploader.addFileData( UIImageJPEGRepresentation(data,0.8)!, withName: currentDate(), withMimeType: "image/jpeg" )
+        }
+        
+        ButterflyFileUploader.sharedUploader.upload()
+        print("ButterflyViewController 's delegate method [-ButterflyViewControllerDidEndReporting] invoked\n")
+    }
+    
+    
+    
+    /// Begin handling shake event.
+    func handleShake(notification: NSNotification) {
+        
+        let screenshot = takeAScreenshot()
+        
+        butterflyViewController = ButterflyViewController()
+        butterflyViewController?.delegate = self
+        butterflyViewController?.image = screenshot
+        
+        var presented = UIApplication.sharedApplication().keyWindow?.rootViewController
+        
+        while let vc = presented?.presentedViewController {
+            presented = vc
+        }
+        
+        let nav = UINavigationController.init(rootViewController: butterflyViewController!)
+        nav.modalTransitionStyle = .CrossDissolve
+        
+        if presented?.isKindOfClass(UINavigationController) == true {
+            let rootvc: UIViewController = (presented as! UINavigationController).viewControllers[0] 
+            if rootvc.isKindOfClass(ButterflyViewController) == false {
+                presented?.presentViewController(nav, animated: true, completion: nil)
+            }
+        } else if presented?.isKindOfClass(ButterflyViewController) == false {
+            presented?.presentViewController(nav, animated: true, completion: nil)
+        }
+    }
+    
+    private func currentDate() -> String! {
+        let sec = NSDate().timeIntervalSinceNow
+        let currentDate = NSDate(timeIntervalSinceNow: sec)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd HH/mm/ss"
+        let string = dateFormatter.stringFromDate(currentDate)
+        return string
+    }
+    
+    //
+    // MARK: - Take screenshot
+    //
+    // http://stackoverflow.com/questions/7964153/ios-whats-the-fastest-most-performant-way-to-make-a-screenshot-programaticall/8017292#8017292
+    
+    internal func takeAScreenshot() -> UIImage? {
+        
+        let orientation = UIApplication.sharedApplication().statusBarOrientation
+        let imageSize: CGSize = UIScreen.mainScreen().bounds.size
+        let layer = UIApplication.sharedApplication().keyWindow?.layer
+        let scale = UIScreen.mainScreen().scale
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, scale);
+        layer?.renderInContext(UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        var imageOrientation: UIImageOrientation
+        switch orientation {
+        case .LandscapeLeft:
+            imageOrientation = UIImageOrientation.Left
+            break
+        case .LandscapeRight:
+            imageOrientation = UIImageOrientation.Right
+            break
+        case .PortraitUpsideDown:
+            imageOrientation = UIImageOrientation.Down
+            break
+        case .Unknown, .Portrait:
+            imageOrientation = UIImageOrientation.Up
+            break
+        }
+        let image = UIImage(CGImage: screenshot.CGImage!, scale: screenshot.scale, orientation: imageOrientation)
+        return image
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(
+            self,
+            name: ButterflyDidShakingNotification,
+            object: nil)
+    }
+}
